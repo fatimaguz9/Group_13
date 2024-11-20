a84cb3cc4e355286b676549c69fd7e311146bdfe
fatimaguz9 <fafaguz2004@gmail.com>
2024-11-20 00:45:03 -0800

    organizing and making mock up of homepage

---
+++ js/site.js
@@ -0,0 +1,11 @@
+// this is the drop down for the recipes 
+
+
+  function toggleDescription() {
+    var desc = document.getElementById('description');
+    if (desc.style.display === 'none') {
+      desc.style.display = 'block';
+    } else {
+      desc.style.display = 'none';
+    }
+  }
