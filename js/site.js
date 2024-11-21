539995ad1e3c2849ee019307aa8901a6c47b3ef8
PapaJohn456 <gotham4069@gmail.com>
2024-11-21 01:29:28 -0800

    Added Lists and randomizer

--- js/site.js
+++ js/site.js
@@ -1,12 +1,20 @@
 // this is the drop down for the recipes 
+var pies= ["Pie","Snickers"];
 
+
+var number= Math.floor(Math.random() * pies.length); 
+
+function generateNew(){
+  number= Math.floor(Math.random() * pies.length); 
+}
 
   function toggleDescription() {
-    var desc = document.getElementById('description');
+    var desc = document.getElementById(pies[number]);
     if (desc.style.display === 'none') {
       desc.style.display = 'block';
     } else {
       desc.style.display = 'none';
+      generateNew()
     }
   }
   
